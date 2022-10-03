dfbdced27df4e79730daaae57806d11dfcb0a049
andres_betancur <65365066+18517701@users.noreply.github.com>
2022-10-02 21:54:23 -0500

    agregar headbar

--- pixiesArg/src/app/features/features.module.ts
+++ pixiesArg/src/app/features/features.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -5,13 +6,14 @@ import { FeaturesRoutingModule } from './features-routing.module';
 import { BottombarComponent } from '../components/bottombar/bottombar.component';
 import { LoginComponent } from './public/login/login.component';
 import { HomeComponent } from './private/home/home.component';
-
+import { HeadbarComponent } from './../components/headbar/headbar.component';
 
 @NgModule({
   declarations: [
     BottombarComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    HeadbarComponent
   ],
   imports: [
     CommonModule,

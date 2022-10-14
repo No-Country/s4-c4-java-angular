576d2679444eab0dc928e9871389f6bd38a3a80c
andres_betancur <65365066+18517701@users.noreply.github.com>
2022-10-13 20:38:30 -0500

    commit coleccion

--- pixiesArg/src/app/features/features.module.ts
+++ pixiesArg/src/app/features/features.module.ts
@@ -7,13 +7,15 @@ import { BottombarComponent } from '../components/bottombar/bottombar.component'
 import { LoginComponent } from './public/login/login.component';
 import { HomeComponent } from './private/home/home.component';
 import { HeadbarComponent } from './../components/headbar/headbar.component';
+import { CollectionComponent } from '../components/collection/collection.component';
 
 @NgModule({
   declarations: [
     BottombarComponent,
     LoginComponent,
     HomeComponent,
-    HeadbarComponent
+    HeadbarComponent,
+    CollectionComponent
   ],
   imports: [
     CommonModule,

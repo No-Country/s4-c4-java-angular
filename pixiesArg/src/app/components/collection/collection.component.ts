eb10a17a45e3fa748a1b140b5aa5c3dc1b3c2fbf
Franco Osores <77215052+filburt88@users.noreply.github.com>
2022-10-14 19:37:24 -0300

    Finished styling head section

--- pixiesArg/src/app/components/collection/collection.component.ts
+++ pixiesArg/src/app/components/collection/collection.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-collection',
   templateUrl: './collection.component.html',
-  styleUrls: ['./collection.component.scss']
+  styleUrls: ['./collection.component.scss'],
 })
 export class CollectionComponent implements OnInit {
+  constructor(private location: Location) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  redirect() {
+    this.location.back();
   }
 
+  ngOnInit(): void {}
 }

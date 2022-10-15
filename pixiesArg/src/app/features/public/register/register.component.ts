22869ba452212961be683a4753addca8b68499e1
Franco Osores <77215052+filburt88@users.noreply.github.com>
2022-10-15 09:52:18 -0300

    Finished layout for register

--- pixiesArg/src/app/features/public/register/register.component.ts
+++ pixiesArg/src/app/features/public/register/register.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { RegisterService } from 'src/app/core/services/register.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup = this.fb.group({
+    firstName: '',
+    lastName: '',
+    country: '',
+    email: '',
+    password: '',
+  });
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private registerService: RegisterService
+  ) {}
 
-  ngOnInit(): void {
+  register() {
+    this.registerService.postRegister(this.registerForm.value).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+    });
   }
 
+  ngOnInit(): void {}
 }
